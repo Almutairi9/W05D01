599991055a727779b7be9466500a33dcad527902
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-11-07 21:20:05 +0300

    toDo task by Node.js

--- app.js
+++ app.js
@@ -1,54 +1,71 @@
-// const { request, response } = require("express");
 const express = require("express");
 
 const app = express();
-const port = 3000; //5000 
 
-app.use(express.json())
+const port = 5000; // OR 3000
+app.use(express.json());
 
-const students = [
-    { name :"Rawan " , age :23 , color :"yellow"}, 
-    { name :"Rana " , age :20 , color :"black"},
-    { name :"Maha " , age :25 , color :"white"},
-    { name :"Razan " , age :15 , color :"pink"},
-]; 
+let toDo = [
+  { id: 1, name: "Rawan", isCompleted: true },
+  { id: 2, name: "Razan", isCompleted: true },
+  { id: 3, name: "Remas", isCompleted: false },
+  { id: 4, name: "Rana", isCompleted: false },
+];
+
+app.get("/disply", (req, res) => {
+  res.status(200);
+  res.json(toDo);
+});
+
+app.get("/disply_Last_Item", (req, res) => {
+  res.status(200);
+  res.json(toDo[3]);
+});
+
+// by params 
+app.post("/add_Name", (req, res) => {
+  toDo.push({ id: 5, name: "Reema", isCompleted: false });
+  res.json(toDo);
+});
+
+app.post("/add_Name_by_body", (req, res) => {
+  const { id, name, isCompleted } = req.body;
+  toDo.push({ id, name, isCompleted });
+  res.json(toDo);
+});
+
+app.get("/completed", (req, res) => {
+  const comp = toDo.filter((e) => e.isCompleted === true);
+  res.json(comp);
+});
+
+app.get("/is_Not_Completed", (req, res) => {
+  const comp = toDo.filter((e) => e.isCompleted === false);
+  res.json(comp);
+});
+
+app.delete("/remove/:id", (req, res) => {
+  const { id } = req.body;
+  toDo.splice(toDo.indexOf({ id }), 1);
+//   res.status(200);
+  res.json(id);
+});
+
+// update 
+app.put("/update/:id/:newName" , (req, res) => {
+    const update = toDo.map((elem) => {
+        if (elem.id == req.params.id ){
+            return{
+                id : elem.id ,
+                name : req.params.newName,
+                isCompleted : elem.isCompleted,
+            };
+        }
+        else return elem;
+    });
+    res.json(update);
+});
     
-app.get("/students", (request,response) =>{
-    response.status(200);
-    response.json(students); 
-})
-
-app.get("/student", (request,response) =>{
-  
-    const { name } = request.query;
-    const found = students.find((e) => {
-        return e.name === name;
-    }); 
-    if (found) {
-        response.status(200); 
-        response.json(found);
-    } 
-    else{
-        response.status(404);
-        response.json("student not found !!");
-    }
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} ...`);
 });
-app.get("/student/:name" , (request,response) =>{
-    const { name } = request.params;
-    const found = students.find(( e) => {
-        return e.name === name;
-    }); 
-})
-
-app.post("/create",(request,response) =>{
-    const {name, age, color} =request.body 
-
-     students.push({name, age, color});
-     response.status(201);
-     response.json({name, age, color});
-});
-
-app.listen(port,() => { 
-
- console.log(`Server is running on port ${port}`);
-}); 
---
+++ index.js
@@ -0,0 +1,72 @@
+//  const { request, response } = require("express");
+// const express = require("express");
+
+// const app = express();
+// const port = 3000; //5000 
+
+// app.use(express.json())
+
+// const students = [
+//     { name :"Rawan " , age :23 , color :"yellow"}, 
+//     { name :"Rana " , age :20 , color :"black"},
+//     { name :"Maha " , age :25 , color :"white"},
+//     { name :"Razan " , age :15 , color :"pink"},
+// ]; 
+    
+// app.get("/students", (request,response) =>{
+//     response.status(200);
+//     response.json(students); 
+// })
+
+// app.get("/student", (request,response) =>{
+  
+//     const { name } = request.query;
+//     const found = students.find((e) => {
+//         return e.name === name;
+//     }); 
+//     if (found) {
+//         response.status(200); 
+//         response.json(found);
+//     } 
+//     else{
+//         response.status(404);
+//         response.json("student not found !!");
+//     }
+// });
+// app.get("/student/:name" , (request,response) =>{
+//     const { name } = request.params;
+//     const found = students.find(( e) => {
+//         return e.name === name;
+//     }); 
+// })
+
+// app.post("/create",(request,response) =>{
+//     const {name, age, color} =request.body 
+
+//      students.push({name, age, color});
+//      response.status(201); 
+//      response.json({name, age, color});
+// });
+
+// app.delete("/remove/color",(request,response) =>{
+//     const {color} = request.body
+//     students.splice(students.indexOf({color}), 1);
+//     response.status(200); 
+//     response.json(color); 
+// });
+
+// // app.put("/name",  (request, response) =>{
+// //    const addWord= (request.body.word, request.body.definition)
+// //     .then( (resp)=> {
+// //         response.send(resp);
+// //       })
+// //       .catch(function (students) {
+// //         console.log(students); 
+// //         response.status(500).send(students);
+// //       });
+// //   }); 
+
+// app.listen(port,() => { 
+
+//  console.log(`Server is running on port ${port}`);
+// }); 
